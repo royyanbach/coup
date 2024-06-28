a9cb7cf5ceb9e8f917a8c412ad398b2b44ec8c34
royyanbach <royyan.bachtiar@gmail.com>
2024-06-29 02:35:18 +0700

    Bad vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  base: command === 'build' ? '/coup/' : '/',
-  plugins: [react()],
-})
+export default defineConfig(({ command, mode }) => {
+  return {
+    base: command === 'build' ? '/coup/' : '/',
+    plugins: [react()],
+  }
+});
