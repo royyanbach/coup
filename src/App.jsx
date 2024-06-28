2a256e51f6f820d9ca2e620e5403422d6838e7a2
royyanbach <royyan.bachtiar@gmail.com>
2024-06-29 02:28:01 +0700

    Update socket address

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3000');
+const socket = io('https://playground.royyanba.ch', {
+  path: '/coup/socket.io'
+});
 
 const configuration = {
   iceServers: [
