fc10d5c084924e9fdb14ad68d155695f6b33d6a7
royyanbach <royyan.bachtiar@gmail.com>
2024-07-01 01:20:28 +0700

    Add missing start icon and update vite build config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ command, mode }) => {
   return {
     base: command === 'build' ? '/coup/' : '/',
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: `[name].[hash].js`,
+          chunkFileNames: `[name].[hash].js`,
+          assetFileNames: `[name].[hash].[ext]`
+        }
+      },
+    },
     plugins: [react()],
   }
 });
